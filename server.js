e2abb9907e6e1cdf79e994874a17cf0f065fcf9d
taeh000n <james1208@mju.ac.kr>
2025-03-30 22:11:23 +0900

    js first server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+// express 라이브러리 사용하겠다는 뜻
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+app.get('/', (요청, 응답) => {
+    응답.send('반갑다')
+})
